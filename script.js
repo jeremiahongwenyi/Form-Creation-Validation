1772f42ef623c69ffa2d9278769b516c0a44a024
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-08-02 09:35:30 +0300

    correction to quiz 0

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('registration-form')
     const feedbackDiv = document.getElementById('form-feedback')
 
-    form.addEventListener('submit', (e) => {
+    form.addEventListener('submit', function (e) {
         e.preventDefault()
     })
 
