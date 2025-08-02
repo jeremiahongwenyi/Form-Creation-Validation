f6320043e90873d2fbe026a2a82c2cf07f3b7b00
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-08-02 09:37:38 +0300

    correction to quiz 0

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     feedbackDiv.style.display = "block";
     if (isValid) {
-        feedbackDiv.textContent = "Registration Successful"
+        feedbackDiv.textContent = "Registration successful"
         feedbackDiv.style.color = "#28a745"
     } else {
         feedbackDiv.innerHTML = messages.join("<br>")
